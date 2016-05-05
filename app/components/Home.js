55e79ca357029f5fd24b8df71824f015984946c1
Ostap Khomitskyi <ostapkhomitskyi@ukr.net>
2016-05-06 01:30:38 +0300

    new commit

---
+++ app/components/Home.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
+var MainWrapper = require('./MainWrapper');
+
+function Home () {
+  return (
+    <MainWrapper>
+      <h1>Github Battle</h1>
+      <p className='lead'>Guess who is stronger?</p>
+      <Link to='/playerOne'>
+        <button type='button' className='btn btn-lg btn-success'>Get Started</button>
+      </Link>
+    </MainWrapper>
+  )
+}
+
+module.exports = Home;
